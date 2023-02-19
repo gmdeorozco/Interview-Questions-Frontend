da4a37752262d941b2e6e357a5348a47a7ab0ab6
Ernesto Orozco <96429978+gmdeorozco@users.noreply.github.com>
2023-02-19 08:27:14 +0000

    added more icons

--- src/components/AvailableTopicsButtons.jsx
+++ src/components/AvailableTopicsButtons.jsx
@@ -1,12 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import { FaJava } from 'react-icons/fa'
 import { FaReact, FaHtml5 } from 'react-icons/fa'
-import { FaGitAlt } from 'react-icons/fa'
+import { FaGitAlt, FaNodeJs } from 'react-icons/fa'
 import { GrMysql } from 'react-icons/gr'
 import { SiSpring, SiSpringboot, SiKubernetes, SiCss3,SiJavascript, SiApachemaven, SiCplusplus } from 'react-icons/si'
 import { FaAws } from 'react-icons/fa'
 import { FiDatabase } from 'react-icons/fi'
-import { GiComputing } from 'react-icons/gi'
+import { GiComputing, GiFrance } from 'react-icons/gi'
 import { BsDiagram3, BsPlusLg } from 'react-icons/bs'
 import { CgTemplate } from 'react-icons/cg'
 import { AiOutlineConsoleSql, AiFillGithub } from 'react-icons/ai'
@@ -14,7 +14,11 @@ import { DiDocker } from 'react-icons/di'
 import { MdOutlineDevices } from 'react-icons/md'
 import  { TiDeleteOutline } from 'react-icons/ti'
 import { useState } from 'react';
-import { SiMongodb, SiSpringsecurity } from 'react-icons/si'
+import { SiMongodb, SiSpringsecurity, SiExpress } from 'react-icons/si';
+import { GrTest } from 'react-icons/gr';
+import { RiMoneyDollarCircleLine } from 'react-icons/ri'
+import { IoPizzaOutline } from 'react-icons/io'
+import { SiJunit5, SiPostgresql, SiRedux } from 'react-icons/si'
 
 
 
@@ -73,8 +77,30 @@ function AvailableTopicsButtons( props ){
                       { theTopic === "GitHub" && <AiFillGithub className = 'me-3' /> }
                       { theTopic === "Software Engeenering" && <MdOutlineDevices className = 'me-3' /> }
                       { theTopic === "Spring" && <SiSpring className = 'me-3' /> }
+                      { theTopic === "Spring JPA" && <SiSpring className = 'me-3' /> }
                       { theTopic === "MongoDB" && <SiMongodb className='me-3' /> }
                       { theTopic === "Spring Security" && <SiSpringsecurity className='me-3' /> }
+                      { theTopic === "Mockito" && < GrTest className='me-3' /> }
+                      { theTopic === "AssertJ" && < GrTest className='me-3' /> }
+                      { theTopic === "MockMVC" && < GrTest className='me-3' /> }
+                      { theTopic === "Spring Test" && < GrTest className='me-3' /> }
+                      { theTopic === "Test Driven Development" && < GrTest className='me-3' /> }
+                      { theTopic === "Jest" && < GrTest className='me-3' /> }
+                      { theTopic === "DataJpaTest" && < GrTest className='me-3' /> }
+                      { theTopic === "Express js" && < SiExpress className='me-3' /> }
+                      { theTopic === "Finances" && < RiMoneyDollarCircleLine className='me-3' /> }
+                      { theTopic === "Francais" && < GiFrance className='me-3' /> }
+                      { theTopic === "Italiano" && < IoPizzaOutline className='me-3' /> }
+                      { theTopic === "JUnit" && < SiJunit5 className='me-3' /> }
+                      { theTopic === "Node JS" && < FaNodeJs className='me-3' /> }
+                      { theTopic === "PostgreDB" && < SiPostgresql className='me-3' /> }
+                      { theTopic === "Redux" && < SiRedux className='me-3' /> }
+                      
+                      
+                      
+                      
+                      
+                      
 
                       
                       
