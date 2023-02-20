7e5f3861540ab8cc124e73342933c550f2b080aa
Ernesto Orozco <96429978+gmdeorozco@users.noreply.github.com>
2023-02-20 04:51:51 +0000

    p

--- src/logic/getMyElo.js
+++ src/logic/getMyElo.js
@@ -1,4 +1,5 @@
 const getMyElo = ( setMyElo, topic ) => {
+    console.log("the topic  *********** " + topic )
     fetch( "https://javainterviewquestions-production.up.railway.app/api/v1/member/elo/1/"+{topic} )
     .then(resp => resp.json())
     .then(data => { 
