36b0f451df7787697712d374e0d3ad1e9218c5d3
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2023-03-06 07:12:48 +0000

    adding

--- src/components/TableOfQuestions.jsx
+++ src/components/TableOfQuestions.jsx
@@ -212,7 +212,7 @@ function TableOfQuestions( props ){
                         } </b>
                         <br/>
                         Last Int:  { 
-                          ( question.lastInteractionDays === 0 || question.lastInteractionDays > 30 ) &&
+                          ( question.lastInteractionDays > 30 ) &&
                           <>
                             < FaHistory />  { question.lastInteractionDays }
                           </>
@@ -221,7 +221,7 @@ function TableOfQuestions( props ){
                           }  
 
                           {
-                            ( question.lastInteractionDays <= 30 && question.lastInteractionDays !== -1 && question.lastInteractionDays !== 0 ) &&
+                            ( question.lastInteractionDays <= 30 && question.lastInteractionDays !== -1  ) &&
                             <>
                                { question.lastInteractionDays }
                             </>
@@ -230,7 +230,7 @@ function TableOfQuestions( props ){
 {
                             ( question.lastInteractionDays === -1 ) &&
                             <>
-                               <i> none </i>
+                               < FaHistory /> <i> none </i>
                             </>
                           }
                        
